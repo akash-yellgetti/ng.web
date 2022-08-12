9c8b6ad11811584e174b9923f7e5f37b63fe99c8
Akash Y <akash.yellgetti@bts.com>
2022-08-12 18:44:32 +0530

    changes for list and grid view layout

--- src/app/modules/admin/user/user.component.ts
+++ src/app/modules/admin/user/user.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MainLayout } from '../../layout/main/main-layout';
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
-
-  constructor() { }
+export class UserComponent extends MainLayout implements OnInit {
+  constructor() { 
+    super();
+  }
 
   ngOnInit(): void {
   }
