88301144e764c7cebda47aee6492948cf4b60c3f
Akash Y <akash.yellgetti@bts.com>
2023-01-05 12:37:44 +0530

    Structural Change

--- src/app/modules/main/main-routing.module.ts
+++ src/app/modules/main/main-routing.module.ts
@@ -1,7 +1,63 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '../auth/guards/auth/auth.guard';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HomeComponent } from './components/home/home.component';
+import { LayoutComponent } from './components/layout/layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'layout',
+    component: LayoutComponent,
+    canActivate: [AuthGuard],
+    // canActivateChild: [AuthGuard],
+    children: [
+      {
+        path: 'home',
+        component: HomeComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: 'admin',
+        loadChildren: () => import('../admin/admin.module').then(m => m.AdminModule)
+      },
+      {
+        path: 'socio-safety',
+        loadChildren: () => import('../socio-safety/socio-safety.module').then(m => m.SocioSafetyModule)
+      },
+      {
+        path: 'service',
+        loadChildren: () => import('../service/service.module').then(m => m.ServiceModule)
+      },
+      {
+          path: 'accounts',
+          loadChildren: () => import('../accounts/accounts.module').then(m => m.AccountsModule)
+      },
+      {
+          path: 'tracker',
+          loadChildren: () => import('../tracker/tracker.module').then(m => m.TrackerModule)
+      },
+      {
+          path: 'communication',
+          loadChildren: () => import('../communication/communication.module').then(m => m.CommunicationModule)
+      },
+      {
+          path: 'user',
+          loadChildren: () => import('../user/user.module').then(m => m.UserModule)
+      },
+      {
+          path: 'market',
+          loadChildren: () => import('../market/market.module').then(m => m.MarketModule)
+      },
+      { path: '',   redirectTo: '/dashboard', pathMatch: 'full' }, // redirect to `first-component`
+      // { path: '**', component: PageNotFoundComponent },  // Wildcard route for a 404 page
+    ]
+  }
+];
+
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/main/main.module.ts
+++ src/app/modules/main/main.module.ts
@@ -3,15 +3,29 @@ import { CommonModule } from '@angular/common';
 
 import { MainRoutingModule } from './main-routing.module';
 import { LayoutComponent } from './components/layout/layout.component';
+import { HelpComponent } from './components/help/help.component';
+import { SettingComponent } from './components/setting/setting.component';
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { BlankComponent } from './components/blank/blank.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { FormBuilderModule } from '../form-builder/form-builder.module';
 
 
 @NgModule({
   declarations: [
-    LayoutComponent
+    LayoutComponent,
+    HelpComponent,
+    SettingComponent,
+    HomeComponent,
+    DashboardComponent,
+    BlankComponent
   ],
   imports: [
     CommonModule,
-    MainRoutingModule
+    MainRoutingModule,
+    FormBuilderModule,
+    SharedModule
   ]
 })
 export class MainModule { }
