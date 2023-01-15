7c8bb5ccd66e4fa9414864ed40c9f54f4af797b9
Chirag Vartak <cvartak26@gmail.com>
2023-01-15 13:28:26 +0530

    WIP: budget page changes

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,20 +1,21 @@
 import { NgModule } from '@angular/core';
 import { MaterialModule } from './modules/material.module';
 import { NgxModule } from './modules/ngx.module';
-
+import { BreadcrumbsComponent } from '../modules/main/components/breadcrumbs/breadcrumbs.component';
 
 
 @NgModule({
   declarations: [
-
+    BreadcrumbsComponent
   ],
   imports: [
     MaterialModule,
-    NgxModule,
+    NgxModule
   ],
   exports: [
     MaterialModule,
     NgxModule,
+    BreadcrumbsComponent
   ]
 })
 export class SharedModule { }
