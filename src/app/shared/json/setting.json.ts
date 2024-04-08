a7754674a77f47985487bf3b617fa9289dee1daa
Akash Yellgetti <akash.yellgetti@bts.com>
2024-04-09 01:37:52 +0530

    New Build #1

--- src/app/shared/json/setting.json.ts
+++ src/app/shared/json/setting.json.ts
@@ -1,4 +1,4 @@
 export const setting = {
-    // uri: 'https://api-web-bxum.onrender.com'
-    uri: 'http://localhost:5001'
+    uri: 'https://api-web-bxum.onrender.com'
+    // uri: 'http://localhost:5001'
 }
