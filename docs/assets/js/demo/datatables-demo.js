3656195b7ef29fbaf8248e20c945afb6c0d97d90
Akash Yellgetti <akash.yellgetti@bts.com>
2024-05-09 17:36:07 +0530

    changes for user name

--- docs/assets/js/demo/datatables-demo.js
+++
@@ -1,9 +0,0 @@
-// Call the dataTables jQuery plugin
-$(document).ready(function() {
-  $('#dataTable').DataTable({
- 
-    dom: 'lBfrtip',
-    // dom: 'lBrf',
-    buttons: ['copy', 'csv', 'excel', 'pdf', 'print'],
-  });
-});
