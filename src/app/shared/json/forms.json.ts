27ff5da2ba2c2e5302f20da6b40178640d123df2
Akash Yellgetti <akash.yellgetti@bts.com>
2024-05-10 14:25:31 +0530

    registration done

--- src/app/shared/json/forms.json.ts
+++ src/app/shared/json/forms.json.ts
@@ -15,6 +15,58 @@ export const forms = {
             "value": "",
         }
     },
+    registrationForm: {
+        firstName: {
+            "name": "firstName",
+            "label": "First Name",
+            "type": "text",
+            "validations": "required",
+            "value": "",
+        },
+        lastName: {
+            "name": "lastName",
+            "label": "Last Name",
+            "type": "text",
+            "validations": "required",
+            "value": "",
+        },
+        gender: {
+            "name": "gender",
+            "label": "Gender",
+            "type": "text",
+            "validations": "required",
+            "value": "",
+        },
+        dob: {
+            "name": "dob",
+            "label": "Date Of Birth",
+            "type": "date",
+            "validations": "required",
+            "value": "",
+        },
+        mobileNo: {
+            "name": "mobileNo",
+            "label": "Mobile No",
+            "type": "date",
+            "validations": "required",
+            "value": "",
+        },
+        
+        email: {
+            "name": "email",
+            "label": "Email",
+            "type": "text",
+            "validations": "required|email",
+            "value": "",
+        },
+        password: {
+            "name": "password",
+            "label": "Password",
+            "type": "text",
+            "validations": "required",
+            "value": "",
+        }
+    },
     // login: [
     //     {
     //         "name": "email",
